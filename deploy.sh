9691f34f022a05e5de2f52eed23e057dd91bb675
Zach Hill <zach@anchore.com>
2019-04-03 15:57:49 -0700

    Change deploy.sh to work against a test bucket.
    Not final setup, path, or deploy operation, but enough to make work visible.

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@ hugo --gc
 
 cp versions.json public/
 
-s3deploy -bucket ${DEST_BUCKET} -path ${PUBLISH_VERSION} -public-access -source public/
+# This is a temp location for testing, to update with version path etc before final version
+s3deploy -bucket anchore-enterprise-docs-test -region us-west-2 -source public/
+
+#s3deploy -bucket ${DEST_BUCKET} -path ${PUBLISH_VERSION} -public-access -source public/
 
