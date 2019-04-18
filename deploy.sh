73b4aee2fa4b499221d38701911f9c14b6ae9ed4
Zach Hill <zach@anchore.com>
2019-04-18 12:02:18 -0700

    Updates to circle config to push branches to specific paths in the site build to support version testing
    Signed-off-by: Zach Hill <zach@anchore.com>

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+path_prefix=${1}
 HUGO_ENV="production"
 
-hugo --gc
 
-cp versions.json public/
+if [[ "$path_prefix" = "master" ]]
+then
+  path_prefix=""
+  hugo --gc -b http://anchore-enterprise-docs-test.s3-website.us-west-2.amazonaws.com/${path_prefix}
+  cp versions.json public/
 
-# This is a temp location for testing, to update with version path etc before final version
-s3deploy -bucket anchore-enterprise-docs-test -region us-west-2 -source public/
+  # This is a temp location for testing, to update with version path etc before final version
+  s3deploy -bucket anchore-enterprise-docs-test -region us-west-2 -source public/
+elif [[ -n "$path_prefix" ]]
+then
+  hugo --gc -b http://anchore-enterprise-docs-test.s3-website.us-west-2.amazonaws.com/${path_prefix}
+  s3deploy -bucket anchore-enterprise-docs-test -region us-west-2 -source public/ -path ${path_prefix}
+else
+  exit 1
+fi
 
 #s3deploy -bucket ${DEST_BUCKET} -path ${PUBLISH_VERSION} -public-access -source public/
 
