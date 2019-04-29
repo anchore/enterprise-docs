c3c881c1b6e9015e1edc6d7779fe3ea332cf1598
Zach Hill <zach@anchore.com>
2019-04-29 13:19:48 -0700

    add deploy check to ensure source dir exists
    Signed-off-by: Zach Hill <zach@anchore.com>

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 set -e
 
 source_dir=${1}
-input_version=${2}
-bucket=${3}
+bucket=${2}
+input_version=${3}
 
 HUGO_ENV="production"
 
-publish_version=$(./get_version.sh)
+publish_version=$(./get_version.sh ${input_version})
 echo "Publish version = ${publish_version}"
 
 if [[ -z "${source_dir}" ]]
@@ -23,17 +23,13 @@ then
   exit 1
 fi
 
-
-# Use the last version in the version file as 'current' and if matches, set up this deploy as current
-latest_version=$(cat versions | tr -s '\n' | tail -n 1)
-echo Current latest version = ${latest_version}
-if [[ "${publish_version}" = ${latest_version} ]]
+if [[ -z ${source_dir} ]]
 then
-  echo "Publish version is current version, also deploying to latest/"
-  s3deploy -bucket ${bucket} -region us-west-2 -source ${source_dir} -path latest/
+  echo No content in source dir, aborting deploy
+  exit 1
+else
+  echo "Deploying contents of ${source_dir} to ${bucket}/${publish_version}"
+  # Add -public-access to make objects public for public site
+  s3deploy -bucket ${bucket} -region us-west-2 -source ${source_dir} -path ${publish_version}
 fi
-
-echo "Deploying contents of ${source_dir} to ${bucket}/${publish_version}"
-# Add -public-access to make objects public for public site
-s3deploy -bucket ${bucket} -region us-west-2 -source ${source_dir} -path ${publish_version}
 
